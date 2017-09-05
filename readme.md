50b0be5d21bb81b8edcd29f54bd7fb763a789d15
Andrei Igna <andrei.igna@layered.studio>
2017-09-05 12:56:02 +0200

    Changed structure of data

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 URL Preview
 =========
 
-**URL Preview** is a PHP library than can get preview info on any URL from the internet! It uses data from [OpenGraph](http://ogp.me/) tags with fallback to HTML meta tags.
+**URL Preview** is a PHP library than can get preview info on any URL from the internet! It uses data from [OpenGraph](http://ogp.me/) tags with fallback to HTML meta tags. No API Keys or Tokens needed
 
 ## Use cases
 
@@ -21,42 +21,64 @@ $ composer require layered/url-preview
 
 #### Usage
 
-For each URL, create a `UrlPreview` instance with URL as first argument. Data is retrieved with `getData()` method:
+For each URL, create a `UrlPreview` instance with URL as first argument. Preview data is retrieved with `getPreview()`, `getProfile()`, `getSite()` and `getEmbed()` methods:
 ```
-$url = new Layered\UrlPreview('https://instagr.am/p/BYOgma8hoUK/');
-$urlData = $url->getData();
+$preview = new Layered\UrlPreview('https://instagr.am/p/BYOgma8hoUK/');
 
-// shorthand method
-$urlData = Layered\UrlPreview::load('https://instagr.am/p/BYOgma8hoUK/')->getData();
+$preview->getPreview();   // preview data
+$preview->getProfile();   // info about user profile / author
+$preview->getSite();      // info about the website
+$preview->getEmbed();     // embed code for URL, if embeddable
 ```
 
 #### Preview data
 
-The returned data from URLs is an Array with the following structure:
+`getPreview()` method returns `Array` with data for URL with following structure:
 ```
 [
   'type'        =>  'photo',     // content type: website, article, photo, etc
-  'url'         =>  'https://www.instagram.com/p/BYOgma8hoUK/',   // processed URL: follows redirects, formatted by site
+  'url'         =>  'https://www.instagram.com/p/BYOgma8hoUK/',   // canonical URL: follows redirects, formatted by site
   'title'       =>  'Instagram post by Andrei • Aug 25, 2017 at 6:23pm UTC',
-  'image'       =>  'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/21149037_113894809286463_3471808960858685440_n.jpg',
-  'description' =>  'Who needs a gym membership when there’s this? Outdoor gym, on the beach #barcelona #beach #gym…'
+  'description' =>  'Who needs a gym membership when there’s this? Outdoor gym, on the beach #barcelona #beach #gym…',
+  'image'       =>  'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/21149037_113894809286463_3471808960858685440_n.jpg'   // URL of preview image or Array with url, width, height for image
 ]
 ```
-Each URL may return extra fields, present in OpenGraph tags or added by a `Layered\UrlPreview\Formatter`. Example of `profile` field for Twitter & Instagram URLS:
 
+#### Profile info
+
+`getProfile()` method returns `false` OR `Array` with info about page author / profile. It contains data present in OpenGraph tags or added by a `Layered\UrlPreview\Formatter`. Example for Twitter & Instagram URLS:
 ```
 [
-  // always present tags
-  'profile'     =>  [
-    'name'        =>  'Andrei',
-    'username'    =>  '@andreihere',
-    'url'         =>  'https://www.instagram.com/andreihere/'
-  ]
+  'name'        =>  'Andrei',
+  'username'    =>  '@andreihere',
+  'url'         =>  'https://www.instagram.com/andreihere/'
 ]
 ```
 
+#### Site info
+
+`getSite()` method returns info about current website. It contains data present in OpenGraph tags or HTML meta tags.
+```
+[
+  'name'        =>  'Instagram',
+  'url'         =>  'https://www.instagram.com',
+  'icon'        =>  'https://www.instagram.com/static/images/ico/favicon-192.png/b407fa101800.png'
+]
+```
+
+#### Embed code
+
+`getEmbed()` method returns `false` OR `Array` with data for embedding the URL. Data is retrieved from oEmbed
+```
+[
+  'width'       =>  658,
+  'height'      =>  null,
+  'html'        =>  '<html>'
+]
+```
 
 ## More
 
 Please report any issues here on GitHub.
+
 [Any contributions are welcome](CONTRIBUTING.md)
--- src/UrlPreview.php
+++ src/UrlPreview.php
@@ -10,30 +10,41 @@ class UrlPreview {
 	private static $previewers = [];
 	private static $formatters = [];
 
-	protected $data = [];
 	protected $previewer = false;
+	protected $crawler = null;
+	protected $url;
+	protected $data = [];
 
 	public function __construct($url) {
-		$client = new Client();
-
 
 		// check for valid URL
 		if (!filter_var($url, FILTER_VALIDATE_URL)) {
 			throw new \Exception('Invalid URL');
 		}
 
+		$this->url = $url;
+
+		return $this;
+	}
+
+	public static function load($url) {
+		return new static($url);
+	}
+
+	protected function process() {
 
 		// load content from URL
-		$crawler = $client->request('GET', $url);
+		$client = new Client();
+		$this->crawler = $client->request('GET', $this->url);
 
 
 		// extract data
 		foreach (self::$previewers as $priority => $previewers) {
 			if (!$this->previewer) {
 				foreach ($previewers as $previewer) {
-					if (!$this->previewer && $previewer->test($crawler)) {
+					if (!$this->previewer && $previewer->test($this->crawler)) {
 						$this->previewer = $previewer;
-						$this->data = $previewer->getData($crawler);
+						$this->data = $previewer->getData($this->crawler);
 						//$this->data['previewer'] = $previewer->getName();
 					}
 				}
@@ -44,22 +55,87 @@ class UrlPreview {
 		// format data
 		foreach (self::$formatters as $priority => $formatters) {
 			foreach ($formatters as $formatter) {
-				if ($formatter->test($crawler, $this->previewer)) {
-					$this->data = $formatter->format($this->data, $crawler, $previewer);
+				if ($formatter->test($this->crawler, $this->previewer)) {
+					$this->data = $formatter->format($this->data, $this->crawler, $previewer);
 				}
 			}
 		}
 
-
-		return $this;
 	}
 
-	public static function load($url) {
-		return new static($url);
-	}
+	public function getAll() {
 
-	public function getData() {
+		if (!$this->crawler) {
+			$this->process();
+		}
+
 		return $this->data;
+	}
+
+	public function getPreview() {
+
+		if (!$this->crawler) {
+			$this->process();
+		}
+
+		$preview = [];
+		$fields = ['type', 'url', 'title', 'description', 'image'];
+
+		foreach ($fields as $field) {
+			$preview[$field] = isset($this->data[$field]) ? $this->data[$field] : '';
+		}
+
+		if (isset($preview['image']) && is_array($preview['image'])) {
+			$preview['image']['url'] = $this->parseImageUrl($preview['url'], $preview['image']['url']);
+		} elseif (isset($preview['image']) && !empty($preview['image'])) {
+			$preview['image'] = $this->parseImageUrl($preview['url'], $preview['image']);
+		}
+
+		return $preview;
+	}
+
+	public function getProfile() {
+
+		if (!$this->crawler) {
+			$this->process();
+		}
+
+		return isset($this->data['profile']) ? $this->data['profile'] : false;
+	}
+
+	public function getSite() {
+
+		if (!$this->crawler) {
+			$this->process();
+		}
+
+		$url = $this->parseUrl($this->crawler->getUri());
+
+		$site = [
+			'url'	=>	$url['scheme'] . '://' . $url['host'],
+			'name'	=>	isset($this->data['site_name']) ? $this->data['site_name'] : '',
+			'icon'	=>	''
+		];
+
+		try {
+			$this->crawler->filter('link[rel=apple-touch-icon]')->each(function($link) use(&$site, $url) {
+				$site['icon'] = $this->parseImageUrl($this->crawler->getUri(), $link->attr('href'));
+			});
+
+			if (empty($site['icon'])) {
+				$this->crawler->filter('link[rel=icon]')->each(function($link) use(&$site, $url) {
+					$site['icon'] = $this->parseImageUrl($this->crawler->getUri(), $link->attr('href'));
+				});
+			}
+		} catch (\Exception $e) {
+			// it's fine
+		}
+
+		return $site;
+	}
+
+	public function getEmbed() {
+		return $this->embedData;
 	}
 
 	public static function addPreviewer($previewer, $priority = 10) {
@@ -68,6 +144,25 @@ class UrlPreview {
 
 	public static function addFormatter($formatter, $priority = 10) {
 		self::$formatters[$priority][] = $formatter;
+	}
+
+	protected function parseUrl($url) {
+		// TODO maybe use a better parser
+		return parse_url($url);
+	}
+
+	protected function parseImageUrl($siteUrl, $imageUrl) {
+
+		if (strpos($imageUrl, 'http') === false) {
+			if ($imageUrl['0'] === '/') {
+				$siteUrl = $this->parseUrl($siteUrl);
+				$imageUrl = $siteUrl['scheme'] . '://' . $siteUrl['host'] . $imageUrl;
+			} else {
+				$imageUrl = rtrim($siteUrl, '/') . '/' . $imageUrl;
+			}
+		}
+
+		return $imageUrl;
 	}
 
 }
